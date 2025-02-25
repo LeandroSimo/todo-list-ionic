12473aeb09bb16226194a89f91d00742b6e825da
LeandroSimo <leo.bt.12@hotmail.com>
2025-02-25 12:19:37 -0300

    feat: [Add home page header]

---
+++ src/presentation/components/TodoHeader .tsx
@@ -0,0 +1,67 @@
+import {
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonText,
+} from "@ionic/react";
+import { add } from "ionicons/icons";
+import { ptBR } from "date-fns/locale";
+import { format } from "date-fns";
+import { setDefaultOptions } from "date-fns/setDefaultOptions";
+
+setDefaultOptions({ locale: ptBR });
+
+const TodoHeader = () => {
+  const currentDate = new Date();
+  const formattedDate = format(currentDate, "EEE', 'd' 'MMM'")
+    .replace(/^\w/, (c) => c.toUpperCase())
+    .replace(
+      /(\w{3})$/,
+      (month) => month.charAt(0).toUpperCase() + month.slice(1)
+    );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        padding: "16px 16px",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <IonText
+          style={{ fontSize: "24px", fontWeight: "bold", color: "black" }}
+        >
+          {formattedDate}
+        </IonText>
+        <IonText>
+          <p style={{ fontSize: "14px", color: "gray", margin: 0 }}>
+            0 tarefas pendentes
+          </p>
+        </IonText>
+      </div>
+      <IonButtons slot="end">
+        <IonButton
+          shape="round"
+          style={{
+            width: "45px",
+            height: "45px",
+            "--border-radius": "50%",
+            "--background": "var(--ion-color-primary)",
+          }}
+        >
+          <IonIcon
+            slot="icon-only"
+            icon={add}
+            style={{ fontSize: "44px", color: "white" }}
+          />
+        </IonButton>
+      </IonButtons>
+    </div>
+  );
+};
+
+export default TodoHeader;
--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,22 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from "@ionic/react";
+import "./Home.css";
+import TodoHeader from "../components/TodoHeader ";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <TodoHeader />
       </IonContent>
     </IonPage>
   );
